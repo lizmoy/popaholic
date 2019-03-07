d525cd6e6ff3efa1f2ceeb0f0889502358c5f743
Elizabeth Moy <elizabethtmoy@gmail.com>
2019-03-07 14:15:11 -0500

    clean code

--- script.js
+++ script.js
@@ -6,14 +6,13 @@ const playAgainBtn = document.querySelector('#replay-button')
 let allPopped = document.querySelectorAll('.popped')
 let allPimples = document.querySelectorAll('.pimple')
 let max = 10;
-let currentMax 
+let currentMax
 let timeCount = 15
-let currentTimeCount 
+let currentTimeCount
 let active = true
 let win = false
 
-function gameInit(){
-    console.log('gameInit is running')
+function gameInit() {
     levelOne.style.display = 'block'
     playAgainBtn.style.display = 'none'
     manyPimples()
@@ -21,14 +20,11 @@ function gameInit(){
 
 startButton.addEventListener('click', function () {
     startTimer(timeCount)
-    console.log('startTimer is running')
 })
 
 function startTimer(seconds) {
-    currentTimeCount = timeCount 
+    currentTimeCount = timeCount
     let timer = setInterval(function () {
-        console.log(`${seconds} is running`)
-        console.log(`${timeCount} is running`)
         seconds -= 1
         currentTimeCount -= 1
         if (seconds === 0) {
@@ -39,12 +35,12 @@ function startTimer(seconds) {
         if (seconds.toString().length === 1) {
             seconds = "0" + seconds
         }
-        if(win === true){
-            console.log('stops timer')
+        if (win === true) {
             clearInterval(timer)
         }
         timerSpan.innerText = "00:" + seconds
     }, 1000);
+
     setTimeout(function () {
         clearInterval(timer)
     }, (seconds * 1000))
@@ -57,9 +53,6 @@ const createPimple = function () {
     document.body.appendChild(newPimple)
     newPimple.style.top = (Math.random() * window.innerHeight) + "px"
     newPimple.style.left = (Math.random() * window.innerWidth) + "px"
-    console.log(`${max} pimples created`)
-    // max = 10
-    // timeCount = 15
     active = true
     win = false
 
@@ -67,9 +60,8 @@ const createPimple = function () {
         newPimple.classList.add('popped')
         setTimeout(() => {
             newPimple.style.display = "none"
-            currentMax -=1
+            currentMax -= 1
             document.body.removeChild(newPimple);
-            console.log(`${currentMax} pimples left`)
             if (currentMax === 0 && active) {
                 youWon()
             }
@@ -78,23 +70,20 @@ const createPimple = function () {
 }
 
 
-let manyPimples = function(){
+let manyPimples = function () {
     currentMax = max
-    console.log('many pimples is running')
     for (let i = 0; i < max; i++) {
         createPimple()
     }
 }
 
 function checkWinLose() {
-    console.log('checkWinLose is running')
     if (currentMax > 0 && currentTimeCount === 0) {
-        console.log('test')
         youLose()
     } gameRestart()
 }
 
-function stopTimer(){
+function stopTimer() {
     clearInterval(startTimer)
 }
 
@@ -102,32 +91,28 @@ function youWon() {
     win = true
     resultMessage.innerHTML = 'You won!'
     gameRestart()
-    console.log('you won is running')
 }
 
 function youLose() {
     stopTimer()
     resultMessage.innerHTML = 'Out of time'
-    console.log('you lose is running')
 }
 
-function gameRestart(){
-    console.log('game restart is running')
+function gameRestart() {
     levelOne.style.display = 'none'
     playAgainBtn.style.display = 'block'
 }
 
-playAgainBtn.addEventListener('click', function(){
+playAgainBtn.addEventListener('click', function () {
     clearPimples()
     gameInit()
     startTimer(timeCount)
     resultMessage.innerHTML = ''
 })
 
-function clearPimples(){
+function clearPimples() {
     allPimples = document.querySelectorAll('.pimple')
-    console.log('clear pimples is running')
-    for(let i=0; i < allPimples.length; i++){
+    for (let i = 0; i < allPimples.length; i++) {
         allPimples[i].remove()
     }
 }
